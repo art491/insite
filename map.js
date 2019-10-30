d6ca7830fe964eb0a5698db24f36886c85838158
art491 <56896568+art491@users.noreply.github.com>
2019-10-29 17:16:20 -0700

    Update map.js

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ let data = [
         content: 'Charlotte'
     },
      {
-        location: [-122.669008, 45.451028],
+        location: [-122.668708, 45.451028],
         content: 'Library: Isabel, Ellory, Justin, Ashley, Dylan'
     },
     {
